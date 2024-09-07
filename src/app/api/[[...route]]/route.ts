59d9ac6c5904d6f4bf49cd6fec0269dea7b6f2c1
gauravmandall <mandalgaurav550@gmail.com>
2024-09-07 23:34:45 +0530

    feat: added RPC routes - added hono RPC user route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,9 +1,23 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { handle } from "hono/vercel";
+import { AuthConfig, initAuthConfig } from "@hono/auth-js";
+import authConfig from "@/auth.config";
+
+// routes
+import userRoutes from "@/app/api/[[...route]]/user";
 
 export const runtime = "nodejs";
 
+function getAuthConfig(c: Context): AuthConfig {
+  return {
+    secret: process.env.SECRET,
+    ...authConfig,
+  };
+}
+
 const app = new Hono().basePath("/api");
+
+const routes = app.route('/user', userRoutes);
 
 app.get("/hello", (c) => {
   return c.json({
@@ -11,7 +25,12 @@ app.get("/hello", (c) => {
   });
 });
 
+app.use("*", initAuthConfig(getAuthConfig));
+
 export const GET = handle(app);
 export const POST = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
+
+
+export type AppType = typeof app;
---
+++ src/app/api/[[...route]]/user.ts
@@ -0,0 +1,28 @@
+import { db } from "@/db/db";
+import { users } from "@/db/schema";
+import { verifyAuth } from "@hono/auth-js";
+import { Hono } from "hono";
+import { eq } from "drizzle-orm";
+
+const app = new Hono().get("/is-premium", verifyAuth(), async (c) => {
+  const session = c.get("authUser");
+  if (!session.token?.email) {
+    return c.json({ error: "Unauthorized" }, 401);
+  }
+
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, session.token.email));
+
+  if (!user) {
+    return c.json({ error: "user not found!" }, 404);
+  }
+  if (!user.isPremium) {
+    return c.json({ isPremium: false });
+  } else {
+    return c.json({ isPremium: true });
+  }
+});
+
+export default app;
